a304a1dc23259a81d2254e7e2590f7fe02d00367
Aarav <ajaaravbvg@gmail.com>
2023-11-16 19:57:20 +0530

    wirtten untested code for linked allocation

--- 2021ucs0080_Aarav_Asgn_9/main.cpp
+++ 2021ucs0080_Aarav_Asgn_9/main.cpp
@@ -17,23 +17,133 @@ class LinkedAllocation {
             int firstBlock; // address of first block
         };
         fileBlock Disk[numOfDiskBlocks];
-        fileBlock Directory[numOfDirectoryEntry];
+        directoryEntry Directory[numOfDirectoryEntry];
 
-    
     public:
         void createFile(string file, int size) {
-            ;
+            // find the first empty entry in the directory
+            directoryEntry* firstEmpty;
+            for(directoryEntry entry: Directory) {
+                if(entry.fileName != "NULL") {
+                    firstEmpty = &entry;
+                    break;
+                }
+            }
+
+            firstEmpty->fileName = file;
+            firstEmpty->fileSize = size;
+
+            // insert datablocks in first fit manner in the disk
+            int diskPtr = 0;
+            int prevDiskPtr = -1;
+            for(int i = 0;i < size;i++) {
+                // find an empty block in the Disk, if empty, the disk block will have prev = -2
+                while(Disk[diskPtr].prev != -2) {
+                    diskPtr++;
+                }
+
+                Disk[diskPtr].prev = prevDiskPtr; // set the prev of new block, prev = -1 represents first block
+                Disk[diskPtr].next = -1; // set it's next to null
+                if(prevDiskPtr != -1) { // if not the first block, set the next of the prev block
+                    Disk[prevDiskPtr].next = diskPtr;
+                }
+                else {
+                    firstEmpty->firstBlock = diskPtr;
+                }
+                prevDiskPtr = diskPtr;
+            }
         }
+
         void deleteFile(string file) {
-            ;
+            // find the directory block with the given file name
+            directoryEntry* fileEntry;
+            for(directoryEntry entry: Directory) {
+                if(entry.fileName != "NULL") {
+                    fileEntry = &entry;
+                    break;
+                }
+            }
+
+            // Traverse the entire linkedlist and make each node null
+            fileBlock* block = &Disk[fileEntry->firstBlock];
+            while(block->next != -1) {
+                block->prev = -2;
+                block = &Disk[block->next];
+            }
         }
-        void changeFile(string file, int newSize) {
-            ;
+
+        void changeFileSize(string file, int newSize) {
+            // find the directory block with the given file name
+            directoryEntry* fileEntry;
+            for(directoryEntry entry: Directory) {
+                if(entry.fileName != "NULL") {
+                    fileEntry = &entry;
+                    break;
+                }
+            }
+            int changeInCount = fileEntry->fileSize - newSize;
+
+            // Traverse the entire linkedlist to get the last block.
+            fileBlock* block = &Disk[fileEntry->firstBlock];
+            while(block->next != -1) {
+                block->prev = -2;
+                block = &Disk[block->next];
+            }
+
+            // After accessing the last block, start truncating/adding the blocks till the newSize is not achieved
+            if(changeInCount > 0) {
+                // truncate blocks
+                for(int i = 0;i < changeInCount;i++) {
+                    int prevPtr = block->prev;
+                    block->prev = -2;
+                    if(prevPtr != -1) {
+                        block = &Disk[prevPtr];
+                    }
+                }
+            }
+            else {
+                // insert datablocks in first fit manner in the disk
+                int diskPtr = 0;
+                int prevDiskPtr = -1;
+                for(int i = 0;i < -changeInCount;i++) {
+                    // find an empty block in the Disk
+                    while(Disk[diskPtr].prev != -2) {
+                        diskPtr++;
+                    }
+
+                    Disk[diskPtr].prev = prevDiskPtr; // set the prev of new block, prev = -1 represents first added block
+                    Disk[diskPtr].next = -1; // set it's next to null
+                    if(prevDiskPtr != -1) { // if not the first block, set the next of the prev block
+                        Disk[prevDiskPtr].next = diskPtr;
+                    }
+                    else {
+                        block->next = diskPtr;
+                        Disk[diskPtr].prev = block - Disk; // index of the lask block
+                    }
+                    prevDiskPtr = diskPtr;
+                }
+            }
         }
+
         void printDiskState() {
-            ;
+            string state = "";
+            for(int i = 0;i < numOfDiskBlocks;i++) { // initialize disk state
+                state += '*';
+            }
+
+            for(directoryEntry &entry: Directory) {
+                int first = entry.firstBlock;
+                while(first != -1) {
+                    state[first] = entry.fileName[0]; // take the first character of the file
+                    first = Disk[first].next;
+                }
+            }
+
+            cout<<state<<endl;
         }
 };
+
+
 class LinkedIndexedAllocation {
     private:
         struct fileBlock {
